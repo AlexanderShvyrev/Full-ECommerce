5cf75cc071c54b206004434f7da43a3351d6a3a0
Alexander Shvyrev <shurik_stv@yahoo.com>
2021-03-23 11:01:06 -0400

    moved collections from shopData to redux

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import CollectionPreview from '../../components/collection-preview/CollectionPreview';
-import SHOP_DATA from './shopData';
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
-export class Shop extends Component {
-  constructor(props) {
-    super(props);
+const Shop = ({ collections }) => {
+  return (
+    <div className='shop-page'>
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
 
-export default Shop;
+export default connect(mapStateToProps)(Shop);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import { persistStore } from 'redux-persist'; //allows to cache our store
+import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
